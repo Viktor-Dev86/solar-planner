b53df114ac6a26cadd4a7d8618212db5f2108750
viktor <iktor.kimster@gmail.com>
2025-11-19 23:31:44 +0200

    Initial project setup

---
+++ src/main/java/com/example/solar_planer/SolarPlanerApplication.java
@@ -0,0 +1,13 @@
+package com.example.solar_planer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarPlanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solar_planer/SolarPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solar_planer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
